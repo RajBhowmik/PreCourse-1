562f53f8be0fe432ad75f6f95460d0369d1b31d1
RajBhowmik <bhowmick.raj10@gmail.com>
2024-07-19 21:44:01 -0700

    Exercies_2_pre1

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,17 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new = ListNode(data)
+        if not self.head:
+            self.head = new
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new
+
+
+
         
     def find(self, key):
         """
@@ -24,9 +37,37 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        curr = self.head
+        while curr:
+            if curr.data == key:
+                return curr
+            curr = curr.next
+        return None
+    
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+        curr = self.head
+        while curr.next:
+            if curr.next.data == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
+
+my_list = SinglyLinkedList()
+
+# Append some elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+found_node = my_list.find(80)
+if found_node:
+    print(f"Found: {found_node.data}")
